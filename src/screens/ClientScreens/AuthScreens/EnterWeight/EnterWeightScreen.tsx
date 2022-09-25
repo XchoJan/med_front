47be6f6c5796ef4a26c8aec78b01b2a431188072
XchoJan <harutyunyanxcho514@gmail.com>
2022-09-25 22:16:20 +0400

    changes

--- src/screens/ClientScreens/AuthScreens/EnterWeight/EnterWeightScreen.tsx
+++ src/screens/ClientScreens/AuthScreens/EnterWeight/EnterWeightScreen.tsx
@@ -13,7 +13,7 @@ import {baseUrl} from "../../../../helpers/url";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const EnterWeightScreen = () => {
-    const NUMBERS:string[] = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,102,102,103,104,105,106,107,108,109,110,111,112,113,114,115'.split(',');
+    const NUMBERS: string[] = '1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95,96,97,98,99,100,102,102,103,104,105,106,107,108,109,110,111,112,113,114,115'.split(',');
     const navigation: any = useNavigation();
     let [userToken, setUserToken] = useState<any>(null)
 
@@ -30,11 +30,16 @@ const EnterWeightScreen = () => {
         AsyncStorage.getItem('userToken').then(r => setUserToken(r))
     }, [])
 
-    async function handleUpdateUserDate(){
+    async function handleUpdateUserDate() {
         let AuthStr = 'Bearer ' + userToken;
         form.append('weight', number)
         try {
-            const response = await axios.put(baseUrl + '/client/update_me/', form, {
+            const response = await axios.put(baseUrl + '/client/update_me/', {
+                body: JSON.stringify({
+                    gender: 'male',
+                    weight: '24'
+                })
+            }, {
                 headers: {
                     'Authorization': AuthStr,
                     'Content-Type': 'multipart/form-data'
@@ -42,12 +47,13 @@ const EnterWeightScreen = () => {
             })
             console.log(response, 'response');
             navigation.navigate('EnterRate')
-        }catch (e){
+        } catch (e) {
             console.log(e, 'error')
         }
 
 
     }
+
     return (
         <ClientContainer>
             <View>
@@ -73,7 +79,7 @@ const EnterWeightScreen = () => {
                     height={300}
                     width={'100%'}
                     initialSelectedIndex={3}
-                    items={NUMBERS.map((name: any) => ({ label: name, value: '' }))}
+                    items={NUMBERS.map((name: any) => ({label: name, value: ''}))}
                     onChange={({item}) => setNumber(item.label)}
                     backgroundColor='#FFFFFF'
                     selectedStyle={{borderWidth: 1}}
